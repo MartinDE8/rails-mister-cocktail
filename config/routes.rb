ba3a610ca95f472e64fdd3c22e26a9257ac127e5
Martin Darras <darras.martin@gmail.com>
2020-08-13 18:11:08 +0100

    Created models, association and validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :ingredients, only: [ :new, :create ]
+  end
+  resources :ingredients do
+    resources :doses
+  end
 end
