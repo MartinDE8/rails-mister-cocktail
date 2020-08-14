3b2c53751cc573ed885193e4e05ee65ddce3050f
Martin Darras <darras.martin@gmail.com>
2020-08-14 16:25:04 +0100

    Created cocktails controller and index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails do
-    resources :ingredients, only: [ :new, :create ]
-  end
-  resources :ingredients do
-    resources :doses
-  end
+  resources :cocktails, only: [:index, :show, :new, :create]
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
 
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+json = JSON.parse(open(url).read)
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+json["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
 
-puts 'Ingredient added...'
+puts "#{Ingredient.count} ingredients created"
